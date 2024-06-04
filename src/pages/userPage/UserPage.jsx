07c8bbad5785e16881ae0fcfdae9840cf5e5cc0e
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-06-04 16:32:00 -0600

    Se crea la vista de usuario logeado

--- src/pages/userPage/UserPage.jsx
+++ src/pages/userPage/UserPage.jsx
@@ -15,10 +15,27 @@ export const UserPage = () => {
   }, []);
 
   return (
-    <div className="user-container">
-      <div className="Marco">
-        <h1>User Page</h1>
-      </div>
+    <div className="view-user-container">
+        <div className="user-container">
+            <div className="img-container">
+                
+            </div>
+            <div className="info-container">
+                <div className="info-user">
+                <img src="../../../public/img/usuario.png" alt="" />
+                  <h1>name </h1>
+                  <p>email </p>
+                  <p>name</p>
+                  <p>phone</p>
+                </div>
+                <div className="info-account">
+                    <h1>account:</h1>
+                    <h2>Ingreso: </h2>
+                    <h2>Balance: </h2>
+                </div>
+            </div>
+            
+        </div>
     </div>
   );
 };
