1fc4b1e783d1da88b62e54532349d9d11cdbe918
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-06-19 17:12:37 -0600

    se muestra la informacion del usuario logueado

--- src/pages/userPage/UserPage.jsx
+++ src/pages/userPage/UserPage.jsx
@@ -7,12 +7,15 @@ import { useUserDetails } from "../../shared/hooks";
 
 import "./userPage.css";
 
-export const UserPage = ({userDetails}) => {
+export const UserPage = () => {
   const { isLogged } = useUserDetails();
 
+  const userDetails = JSON.parse(localStorage.getItem('userDetails'));
+
   useEffect(() => {
-    (isLogged);
-  }, []);
+    console.log('El usuario es:', userDetails);
+}, [userDetails]);
+
 
   console.log(userDetails, 'us')
 
@@ -25,17 +28,17 @@ export const UserPage = ({userDetails}) => {
             <div className="info-container">
                 <div className="info-user">
                 <img src="../../../public/img/user.png" alt="" />
-                  <h1>{userDetails.name}</h1>
-                  <p>email </p>
-                  <p>name</p>
-                  <p>phone</p>
+                  <h1>User</h1>
+                  <p>{userDetails ? userDetails.email : ''}</p>
+                  <p>{userDetails ? userDetails.name : ''}</p>
+                  <p>{userDetails ? userDetails.phone : ''}</p>
                 </div>
                 <hr />
                 <hr />
                 <div className="info-account">
-                  <h1>account: <span>4</span> </h1>
-                  <h2>Ingreso: <span>Q3</span></h2>
-                  <h2>Balance: <span>Q4</span></h2>
+                <h1>account: <span>{userDetails ? userDetails.account : ''}</span> </h1>
+                <h2>Ingreso: <span>Q{userDetails ? userDetails.salary : ''}</span></h2>
+                <h2>Balance: <span>Q{userDetails ? userDetails.balance : ''}</span></h2>
                 </div>
             </div>
             
