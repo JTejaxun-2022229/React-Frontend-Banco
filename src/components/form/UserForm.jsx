8b63f298eb412d503ea0c54a49f19a2d7940d43d
gchuquiej2022273 <gchuquiej-2022273@kinal.edu.gt>
2024-07-08 00:24:57 -0600

    Se comenzo en Credits se eliminaron cosas que no servian pero ahí estaban, se coloco el ojito el el password de login y se coloco una funcion al momento de darle click

--- src/components/form/UserForm.jsx
+++ src/components/form/UserForm.jsx
@@ -1,33 +1,82 @@
-import iconStore from "../../assets/img/iconStore.png"
-import IconTransaction from "../../assets/img/IconTransaction.png"
-import iconCredit from "../../assets/img/IconCredit.png"
-import iconFavorite from "../../assets/img/iconFavorite.png"
+import iconStore from "../../assets/img/iconStore.png";
+import IconTransaction from "../../assets/img/IconTransaction.png";
+import iconCredit from "../../assets/img/IconCredit.png";
+import iconFavorite from "../../assets/img/iconFavorite.png";
+import iconHome from "../../assets/img/IconHome.png";
+
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 /**Retorna el formulalio del las opciones de Usuario*/
-/**Agregar opciones segun necesite */
 export const UserSidebar = () => {
-    return(
-        <div className='button-sidebar-container'>
-        <ul>
-            <li><img src={iconStore} alt="iconStore" /><a>Store</a></li>
-            <li><img src={iconCredit} alt="iconCredit" /><a>Credit</a></li>
-            <li><img src={IconTransaction} alt="iconTransaction" /><a>Transaction</a></li>
-            <li><img src={iconFavorite} alt="iconFavorite" /><a>Favorite</a></li>
-        </ul>
-    </div>
-    )
-}
+  const [activeIndex, setActiveIndex] = useState(null);
 
-export const UserSidebarRight = () =>{
-    return(
-        <div className='input-sidebar-container'>
-            <ul >
-                <input className="form-sidebar-button" type="button" value="Profile"/>
-                <input className="form-sidebar-button" type="button" value="Setting"/>
-                <input className="form-sidebar-button" type="button" value="History"/>
-                <input className="form-sidebar-button" type="button" value="Help"/>
-                
-            </ul>
-        </div>
-    )
-}
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="button-sidebar-container">
+      <ul>
+        <li
+          className={`button-sidebar-container ${
+            activeIndex === 0 ? "active" : ""
+          }`}
+          onClick={() => handleClick(0)}
+        >
+          <img src={iconHome} alt="iconClients" />
+          <NavLink to="/dashboard/home">Home</NavLink>
+        </li>
+        <li
+          className={`button-sidebar-container ${
+            activeIndex === 1 ? "active" : ""
+          }`}
+          onClick={() => handleClick(1)}
+        >
+          <img src={iconStore} alt="iconClients" />
+          <NavLink to="/dashboard/Store">Store</NavLink>
+        </li>
+        <li
+          className={`button-sidebar-container ${
+            activeIndex === 2 ? "active" : ""
+          }`}
+          onClick={() => handleClick(2)}
+        >
+          <img src={iconCredit} alt="iconCreateClient" />
+          <NavLink to="/dashboard/applyCredit">Credit</NavLink>
+        </li>
+        <li
+          className={`button-sidebar-container ${
+            activeIndex === 3 ? "active" : ""
+          }`}
+          onClick={() => handleClick(3)}
+        >
+          <img src={IconTransaction} alt="iconCreateAdmin" />
+          <NavLink to="/dashboard/Transaction">Transaction</NavLink>
+        </li>
+        <li
+          className={`button-sidebar-container ${
+            activeIndex === 4 ? "active" : ""
+          }`}
+          onClick={() => handleClick(4)}
+        >
+          <img src={iconFavorite} alt="iconProductList" />
+          <NavLink to="/dashboard/products">Favorite</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export const UserSidebarRight = () => {
+  return (
+    <div className="input-sidebar-container">
+      <ul>
+        <input className="form-sidebar-button" type="button" value="Profile" />
+        <input className="form-sidebar-button" type="button" value="Setting" />
+        <input className="form-sidebar-button" type="button" value="History" />
+        <input className="form-sidebar-button" type="button" value="Help" />
+      </ul>
+    </div>
+  );
+};
--- src/components/navbar/Sidebar.jsx
+++ src/components/navbar/Sidebar.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { AdminSidebar } from '../form/AdminForm.jsx';
 import { UserSidebar } from '../form/UserForm.jsx';
 import logo from "../../assets/img/LogoQuetzalito_Principal.png"
+import { useUserDetails } from '../../shared/hooks/useUserDetails.jsx';
+
 
 const Logo = () => {
     return (
@@ -12,23 +14,15 @@ const Logo = () => {
 const Name = ({ name }) => {
     return (
         <div className="name-container">
-            <h1>Welcome</h1>
+            <h1>Welcome!</h1>
             <h3>{name}</h3>
         </div>
     );
 }
 
 export const Sidebar = () => {
-    const [role, setRole] = useState('');
-    const [name, setName] = useState('');
 
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem('user'));
-        if (user) {
-            setRole(user.role);
-            setName(user.name)
-        }
-    }, []);
+    const {username, role} = useUserDetails();
 
     return (
         <div className="sidebar-container">
@@ -36,7 +30,7 @@ export const Sidebar = () => {
                 <Logo />
             </div>
             <div className="sidebar-title-name-container">
-                <Name name={name} />
+                <Name name={username} />
             </div>
             <div className="sidebar-menu">
                 {role === 'ADMIN_ROLE' ? (
