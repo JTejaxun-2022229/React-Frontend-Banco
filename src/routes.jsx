95839dda94ccb20388f0ecb7d0296dbaea516858
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-07-09 17:20:39 -0600

    Nueva ruta

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { UserPage } from "./pages/userPage";
 import { AdminPage } from "./pages/adminPage";
 import { AuthPage } from "./pages/auth";
+import {Conversor} from './pages/converter'
 
 const routes = [
     { path: '/', element: <AuthPage /> },
     { path: '/user', element: <AdminPage /> },   
     { path: '/admin', element: <AdminPage /> }, 
+    { path: '/convertion', element: <Conversor />}
 ];
 
 export default routes;
