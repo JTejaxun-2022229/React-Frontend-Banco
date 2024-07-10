e3dc0f7755a6c211951944e0cbad24052faf196c
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-07-09 23:02:52 -0600

    Se arregla el conversor y se agrega color de fondo

--- src/pages/converter/Conversor.jsx
+++ src/pages/converter/Conversor.jsx
@@ -1,98 +1,107 @@
 import React, { useState } from 'react';
 import axios from "axios";
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './conversor.css'
 
 export const Conversor = () => {
     const [formData, setFormData] = useState({
         from: "",
-        to:"",
-        amount:""
+        to: "",
+        amount: "",
     });
+    const [result, setResult] = useState(null);
+    const [error, setError] = useState("");
 
-    const [ result, setResult ] = useState(null);
-    const [error, setError ] = useState('');
-
-    const currencyCodes = ['GTQ','EUR','MXN',"USD","HNL","NIO","CRC","CAD"]
+    const currencyCodes = ["GTQ", "EUR", "MXN", "USD", "HNL", "NIO", "CRC","CAD"];
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+    }));
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try{
-            const respon = await axios.post(
-                'http://127.0.0.1:4000/quetzalito/v1/convert/',
-                formData
-            );
-
-            setResult(response?.data);
-            setError('');
-        }catch(error){
-            setError(
-                'Error',
-                error?.response ? error?.response?.data : error?.message
-            );
-        };
+    e.preventDefault();
+    //Http Request
+    try {
+        const response = await axios.post(
+        "http://localhost:8080/api/v1/convert ", 
+        formData
+        );
+        setResult(response?.data);
+        setError("");
+    } catch (error) {
+        setError(
+        "Error",
+        error?.response ? error?.response?.data : error?.message
+        );
+    }
     };
 
-    return(
+    const goBack = () => {
+    window.location.reload();
+    };
+    
+    return (
         <div>
-            <section className='conversor'>
-                <form onSubmit={handleSubmit}>
-                    <select 
-                        name="from"
-                        value={formData.from}
-                        onChange={handleChange}
-                        classname='imput'
-                    >
-                        <option>Select from Currency</option>
-                        {currencyCodes.map((code) => (
-                            <option value={code} key={code}>
-                                {code}
-                            </option>
-                        ))}
-                    </select>
-                    <select name="to" 
-                        value={formData.to}
-                        onChange={handleChange}
-                        classname='imput
-                    '>
-                        {currencyCodes.map((code) => (
-                            <option key={code} value={code}>
-                            {code}
-                            </option>
-                        ))}
-                    </select>
-                    <input
-                    name="amount"
-                    value={formData.amount}
-                    onChange={handleChange}
-                    placeholder="Amount"
-                    type="number"
-                    className="input"
-                    />
-                    <button type="submit" className="submit-btn">
-                    Convert
-                    </button>
-                </form>
-                {result && (
-                    <div className="result">
-                    <p>
-                        Converted Amount: {result.convertedAmount} {result.target}
-                    </p>
-                    <p>Conversion Rate: {result.conversionRate}</p>
-                    </div>
-                )}
-                {error && <p className="error">Error: {error}</p>}
-            </section>
-        </div>
-    )
+        <section className="conversor">
+        <button className="close-button" onClick={goBack}>
+            <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <form onSubmit={handleSubmit}>
+            <select
+            name="from"
+            value={formData.from}
+            onChange={handleChange}
+            className="input"
+            >
+            <option value="">Select From Currency</option>
+            {currencyCodes.map((code) => (
+                <option key={code} value={code}>
+                {code}
+                </option>
+            ))}
+            </select>
+            <select
+            name="to"
+            value={formData.to}
+            onChange={handleChange}
+            className="input"
+            >
+            <option value="">Select To Currency</option>
+            {currencyCodes.map((code) => (
+                <option key={code} value={code}>
+                {code}
+                </option>
+            ))}
+            </select>
+            <input
+            name="amount"
+            value={formData.amount}
+            onChange={handleChange}
+            placeholder="Amount"
+            type="number"
+            className="input"
+            />
+            <button type="submit" className="submit-btn">
+            Convert
+            </button>
+        </form>
+        {result && (
+            <div className="result">
+            <p>
+                Converted Amount: {result.convertedAmount} {result.target}
+            </p>
+            <p>Conversion Rate: {result.conversionRate}</p>
+            </div>
+        )}
+        {error && <p className="error">Error: {error}</p>}
+        </section>
+    </div>
+    );
 
 };
